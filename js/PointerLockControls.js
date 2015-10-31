13a544fe86dd4cd50cbbbeb6e641895622d34fe9
Cornell Daly <cmdaly@stetson.edu>
2015-10-31 16:25:42 -0400

    Removed unnecessary code from PointerLockControls.

--- js/PointerLockControls.js
+++ js/PointerLockControls.js
@@ -10,27 +10,9 @@ PointerLockControls = Class({
     this.yawObject.position.y = 10;
     this.yawObject.add(this.pitchObject);
 
-    this.moveForward = false;
-    this.moveBackward = false;
-    this.moveLeft = false;
-    this.moveRight = false;
-
-    this.isOnObject = false;
-    this.canJump = false;
-
-    this.prevTime = performance.now();
-
-    this.velocity = new THREE.Vector3();
-
     this.enabled = false;
 
-    this.mass = options.mass || 100.0;
-    this.moveSpeed = options.moveSpeed || new THREE.Vector3(400.0, 350.0, 400.0);
-    this.minimumY = options.minimumY || 10;
-
     document.addEventListener('mousemove', this.onMouseMove.bind(this), false);
-    document.addEventListener('keydown', this.onKeyDown.bind(this), false);
-    document.addEventListener('keyup', this.onKeyUp.bind(this), false);
   },
 
   onMouseMove: function(event) {
@@ -47,76 +29,8 @@ PointerLockControls = Class({
     this.pitchObject.rotation.x = Math.max(-PointerLockControls.PI_2, Math.min(PointerLockControls.PI_2, this.pitchObject.rotation.x));
   },
 
-  onKeyDown: function(event) {
-    switch (event.keyCode) {
-      case 38: // up
-      case 87: // w
-        this.moveForward = true;
-        break;
-
-      case 37: // left
-      case 65: // a
-        this.moveLeft = true;
-        break;
-
-      case 40: // down
-      case 83: // s
-        this.moveBackward = true;
-        break;
-
-      case 39: // right
-      case 68: // d
-        this.moveRight = true;
-        break;
-
-      case 32: // space
-        if (this.canJump === true) {
-          this.velocity.y += this.moveSpeed.y;
-        }
-
-        this.canJump = false;
-
-        break;
-
-      default:
-        break;
-    }
-  },
-
-  onKeyUp: function(event) {
-    switch (event.keyCode) {
-      case 38: // up
-      case 87: // w
-        this.moveForward = false;
-        break;
-
-      case 37: // left
-      case 65: // a
-        this.moveLeft = false;
-        break;
-
-      case 40: // down
-      case 83: // s
-        this.moveBackward = false;
-        break;
-
-      case 39: // right
-      case 68: // d
-        this.moveRight = false;
-        break;
-
-      default:
-        break;
-    }
-  },
-
   getObject: function() {
     return this.yawObject;
-  },
-
-  isOnObject: function(bool) {
-    this.isOnObject = bool;
-    this.canJump = bool;
   },
 
   getDirection: function() {
@@ -131,47 +45,8 @@ PointerLockControls = Class({
 
       return v;
     };
-  }(),
+  }()
 
-  update: function(dt) {
-    if (this.enabled === false) {
-      return;
-    }
-
-    this.velocity.x -= this.velocity.x * 10.0 * dt;
-    this.velocity.z -= this.velocity.z * 10.0 * dt;
-
-    this.velocity.y -= 9.8 * this.mass * dt;
-
-    if (this.moveForward) {
-      this.velocity.z -= this.moveSpeed.z * dt;
-    }
-    if (this.moveBackward) {
-      this.velocity.z += this.moveSpeed.z * dt;
-    }
-
-    if (this.moveLeft) {
-      this.velocity.x -= this.moveSpeed.x * dt;
-    }
-    if (this.moveRight) {
-      this.velocity.x += this.moveSpeed.x * dt;
-    }
-
-    if (this.isOnObject === true) {
-      this.velocity.y = Math.max(0, this.velocity.y);
-    }
-
-    this.yawObject.translateX(this.velocity.x * dt);
-    this.yawObject.translateY(this.velocity.y * dt);
-    this.yawObject.translateZ(this.velocity.z * dt);
-
-    if (this.yawObject.position.y < this.minimumY) {
-      this.velocity.y = 0;
-      this.yawObject.position.y = this.minimumY;
-
-      this.canJump = true;
-    }
-  }
 }, {
   statics: {
     PI_2: Math.PI / 2
